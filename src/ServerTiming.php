12edb1ac363d4ba3593fda2b2a254a7b569d0280
Emanuele "ToX" Toscano <toss82@gmail.com>
2024-07-23 09:27:05 +0200

    Code improvements

--- src/ServerTiming.php
+++ src/ServerTiming.php
@@ -5,19 +5,21 @@ declare(strict_types=1);
 namespace Tox82;
 
 /**
- * ServerTiming helper class
- * usage: simply call ServerTiming::start('metricName') and ServerTiming::stop('metricName') in your code
- * to measure the time between the two calls
- * You can have multiple metrics running at the same time
- * The metrics will be sent to the client in the Server-Timing header
- * The client can then use this information to display the metrics in the browser
- * See https://www.w3.org/TR/server-timing/ for more information
+ * ServerTiming helper class.
  *
- * You will see the metrics in the Network tab of the Chrome DevTools, under the Timing tab
+ * Usage:
+ * Simply call `ServerTiming::start('metric')` and `ServerTiming::stop('metric')` in your code
+ * to measure the time between the two calls.
+ * You can have multiple metrics running at the same time.
+ * The metrics will be sent to the client in the Server-Timing header.
+ * The client can then use this information to display the metrics in the browser.
+ * See https://www.w3.org/TR/server-timing/ for more information.
+ *
+ * You will see the metrics in the Network tab of the Chrome DevTools, under the Timing tab.
  *
  * @category ServerTiming helper class
  * @package  Tox82\ServerTiming
- * @author  Emanuele "ToX" Toscano <https://emanuele.itoscano.com>
+ * @author   Emanuele "ToX" Toscano <https://emanuele.itoscano.com>
  * @license  GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
  */
 class ServerTiming
@@ -26,71 +28,104 @@ class ServerTiming
     private static $totals = [];
 
     /**
-     * Starts the timer for the given metric name
+     * Starts the timer for the given metric.
      *
-     * @param string $metricName The name of the metric
+     * @param string $metric The name of the metric.
      * @return void
      */
-    public static function start(string $metricName)
+    public static function start(string $metric): void
     {
-        self::$timers[$metricName] = microtime(true);
+        // Set the timer for the given metric
+        self::$timers[$metric] = microtime(true);
 
-        if (!isset(self::$totals['calls'][$metricName])) {
-            self::$totals['calls'][$metricName] = 0;
-            self::$totals['time'][$metricName] = 0;
-            self::$totals['start'][$metricName] = microtime(true);
+        // If this is the first time we're tracking this metric, initialize the totals array
+        if (! isset(self::$totals[$metric])) {
+            self::$totals[$metric]['count'] = 0;
+            self::$totals[$metric]['time'] = 0;
+            self::$totals[$metric]['start'] = microtime(true);
         }
     }
 
     /**
-     * Stops the timer for the given metric name and sends the metric to the client in the Server-Timing header
+     * Stops the timer for the given metric and sends the metric to the client in the Server-Timing header.
      *
-     * @param string $metricName The name of the metric
-     * @return float The duration in milliseconds
+     * @param string $metric The name of the metric.
+     * @return float The duration in milliseconds.
      */
-    public static function stop(string $metricName)
+    public static function stop(string $metric): float
     {
-        $time = self::$timers[$metricName];
-        unset(self::$timers[$metricName]);
+        $time = self::$timers[$metric];
+        unset(self::$timers[$metric]);
 
-        self::$totals['calls'][$metricName] += 1;
-        self::$totals['time'][$metricName] += (microtime(true) - $time) * 1000;
+        self::$totals[$metric]['count'] += 1;
+        self::$totals[$metric]['time'] += self::getDurationInMicroseconds($time);
 
-        return (microtime(true) - $time) * 1000;
+        return self::getDurationInMilliseconds($time);
     }
 
     /**
-     * Stops the timer for the given metric name if it is running, otherwise starts it
+     * Stops the timer for the given metric if it is running, otherwise starts it.
      *
-     * @param string $metricName The name of the metric
+     * @param string $metric The name of the metric.
      * @return void
      */
-    public static function profile(string $metricName)
+    public static function profile(string $metric): void
     {
-        if (isset(self::$timers[$metricName])) {
-            $durationInMicroseconds = self::stop($metricName);
-            header("Server-Timing: " . $metricName . ";dur=" . $durationInMicroseconds . ";desc=" . $metricName);
+        if (isset(self::$timers[$metric])) {
+            $durationInMicroseconds = self::stop($metric);
+            header('Server-Timing: ' . $metric . ';dur=' . $durationInMicroseconds . ';desc=' . $metric);
         } else {
-            self::start($metricName);
+            self::start($metric);
         }
     }
 
     /**
-     * Log method: this calculates the time between the start and stop calls, and triggers an error_log for both the start and the stop times, with the time difference in the stop message
+     * Calculates the time between the start and stop calls and logs the duration.
      *
-     * @param string $metricName The name of the metric
+     * @param string $metric The name of the metric.
      * @return void
      */
-    public static function log(string $metricName)
+    public static function log(string $metric): void
     {
-        if (isset(self::$timers[$metricName])) {
-            $durationInMicroseconds = self::stop($metricName);
-            $totalCalls = self::$totals['calls'][$metricName];
-            $totalInMicroseconds = self::$totals['time'][$metricName];
-            error_log('Server-Timing: ' . $metricName . " stop - (" . round($durationInMicroseconds, 5) . " ms - called " . $totalCalls . " times, total time: " . round($totalInMicroseconds, 5) . " ms)");
+        if (isset(self::$timers[$metric])) {
+            $durationInMicroseconds = self::stop($metric);
+            $count = self::$totals[$metric]['count'];
+            $totalTimeInMicroseconds = self::$totals[$metric]['time'];
+            $durationInMilliseconds = self::getDurationInMilliseconds($durationInMicroseconds);
+            $totalTimeInMilliseconds = self::getDurationInMilliseconds($totalTimeInMicroseconds);
+            error_log(sprintf(
+                'Server-Timing: %s stop - (%s ms - called %d times, total time: %s ms)',
+                $metric,
+                round($durationInMilliseconds, 5),
+                $count,
+                round($totalTimeInMilliseconds, 5)
+            ));
         } else {
-            self::start($metricName);
-            error_log('Server-Timing: ' . $metricName . " start");
+            self::start($metric);
+            error_log('Server-Timing: ' . $metric . " start");
         }
+    }
+
+
+    /**
+     * Calculates the duration in microseconds between the given start time and the current time.
+     *
+     * @param float $startTime The start time in seconds with microseconds.
+     * @return float The duration in microseconds.
+     */
+    private static function getDurationInMicroseconds(float $startTime): float
+    {
+        return (microtime(true) - $startTime) * 1000000;
+    }
+
+    /**
+     * Calculates the duration in milliseconds between the given start time and the current time.
+     *
+     * @param float $startTime The start time in seconds with microseconds.
+     * @return float The duration in milliseconds.
+     */
+    private static function getDurationInMilliseconds(float $startTime): float
+    {
+        return self::getDurationInMicroseconds($startTime) / 1000;
     }
 }
