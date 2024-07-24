bf112da155601ca9a7b21724532b40e66e8187f0
Emanuele "ToX" Toscano <toss82@gmail.com>
2024-07-24 17:11:22 +0200

    Added log file detection and logging functionality to ServerTiming class
    - Introduced `detectLogFile` method to automatically find a suitable log file path from common locations.
    - Enhanced `log` method to detect and use the log file for logging metrics.
    - Added `writeLog` method to handle writing messages to the log file.
    - Implemented `setLogFile` method to allow manual setting of the log file path with validation.
    - Updated `start`, `stop`, and `profile` methods to support logging functionality.

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # Server Timing Helper
-## A simple tool that you can use to benchmark your PHP code
+## A Simple Tool for Benchmarking Your PHP Code
 
-This tool provides an easy way to benchmark your PHP code. It is based on the [Server Timing](https://www.w3.org/TR/server-timing/) specification. You can read more about it on [web.dev](https://web.dev/custom-metrics/?utm_source=devtools#server-timing-api).
+The `ServerTiming` class is designed to assist developers in measuring and logging the execution time of various code segments within a PHP application. By utilizing this class, you can initiate and terminate timers for different metrics, transmit these metrics to the client via the Server-Timing header, and record the results to a file. This is particularly useful for performance monitoring and optimization.
 
-Installation:
+![server-timing](https://user-images.githubusercontent.com/659492/216758847-673f1155-db52-48a8-aada-3648b7c837cf.png)
+
+This tool is based on the [Server Timing](https://www.w3.org/TR/server-timing/) specification. You can learn more about it on [web.dev](https://web.dev/custom-metrics/?utm_source=devtools#server-timing-api).
+
+# Installation:
+-----------
 ```shell
 composer require tox82/server-timing-helper
 ```
 
-Usage
+# Usage
 -----------
 
-Let's say you want to benchmark the execution of a function. You can do it like this:
+## The profile Method
+This is the easiest way to use the `ServerTiming` class. It automatically handles the start/stop methods, and logs the results to the `Network/Timing` panel in your browser's `DevTools`.
 
 ```php
 \Tox82\ServerTiming::profile('metricName');
@@ -19,33 +25,41 @@ Let's say you want to benchmark the execution of a function. You can do it like 
 \Tox82\ServerTiming::profile('metricName');
 ```
 
-And that's it! You will see the metrics in the Network tab of your browser's DevTools, under the Timing tab
-
-![server-timing](https://user-images.githubusercontent.com/659492/216758847-673f1155-db52-48a8-aada-3648b7c837cf.png)
-
-I don't like the ServerTiming profiler!
------------
-
-You can track the metrics in your log files too. Just use the `log` method instead:
+## The start/stop Method
+Alternatively, you can manually control the start and stop methods by using `start/stop` instead of `profile`:
 
 ```php
-\Tox82\ServerTiming::log('metricName');
-\Tox82\ServerTiming::log('    firstBlock');
+\Tox82\ServerTiming::start('metricName');
 # ... your code here ...
-\Tox82\ServerTiming::log('    firstBlock');
-\Tox82\ServerTiming::log('    secondBlock');
-# ... your code here ...
-\Tox82\ServerTiming::log('    secondBlock');
-\Tox82\ServerTiming::log('metricName');
+\Tox82\ServerTiming::stop('metricName');
 ```
 
-And you will see something like this in your apache's error log
+## Saving the Output to a Log File
+The `log` method enables you to write detailed log entries for your metrics to a file. 
+This requires you to have a valid, writeable log file, but IMO it makes it a little bit easier to read the metrics.
+You can use nested blocks to log the execution time of specific parts of your code. My suggestion is to add some spaces to the beginning of the nested blocks to make it more readable.
 
-![image](https://user-images.githubusercontent.com/659492/229862330-11b94c17-561d-45eb-8393-aed2a13508ea.png)
+```php
+\Tox82\ServerTiming::log('heavyBlocks');
+# foreach ($items as $item) {
+\Tox82\ServerTiming::log('    singleBlock');
+# ... your code here ...
+\Tox82\ServerTiming::log('    singleBlock');
+# }
+\Tox82\ServerTiming::log('heavyBlocks');
+```
+![server_timing](https://github.com/user-attachments/assets/e8c46310-1671-4239-86a6-7da293e5d371)
 
 
+## Setting a Custom Log File
 
-Resources
+By default, `Server Timing Helper` attempts to find the apache error_log file path for logging. However, you can specify a custom log file path using the `setLogFile` method. Make sure to set the log file path before using the `log` method.
+
+```php
+\Tox82\ServerTiming::setLogFile('/var/www/server_timing.log');
+```
+
+# Resources
 ---------
  * [Report issues](https://github.com/ToX82/server-timing-helper/issues)
  * [Send Pull Requests](https://github.com/ToX82/server-timing-helper/pulls)
--- src/ServerTiming.php
+++ src/ServerTiming.php
@@ -26,6 +26,36 @@ class ServerTiming
 {
     private static $timers = [];
     private static $totals = [];
+    private static $logFile;
+
+    /**
+     * Attempts to detect a suitable log file path based on common locations.
+     * 
+     * This method iterates through a list of common log file paths to find the first one that exists and is writable.
+     * If no suitable log file is found, $logFile is set to null.
+     * 
+     * @return void
+     */
+    public static function detectLogFile(): void
+    {
+        // Common log file paths
+        $commonLogPaths = [
+            '/var/log/apache2/error.log',
+            '/var/log/httpd/error.log',
+            'C:/Apache24/logs/error.log',
+            '/Applications/MAMP/logs/apache_error.log',
+            '/Applications/MAMP/logs/error.log',
+        ];
+
+        foreach ($commonLogPaths as $path) {
+            if (file_exists($path) && is_writable($path)) {
+                self::$logFile = $path;
+                return;
+            }
+        }
+
+        self::$logFile = null;
+    }
 
     /**
      * Starts the timer for the given metric.
@@ -40,9 +70,7 @@ class ServerTiming
 
         // If this is the first time we're tracking this metric, initialize the totals array
         if (! isset(self::$totals[$metric])) {
-            self::$totals[$metric]['count'] = 0;
-            self::$totals[$metric]['time'] = 0;
-            self::$totals[$metric]['start'] = microtime(true);
+            self::$totals[$metric] = ['count' => 0, 'time' => 0, 'start' => microtime(true)];
         }
     }
 
@@ -54,13 +82,18 @@ class ServerTiming
      */
     public static function stop(string $metric): float
     {
-        $time = self::$timers[$metric];
-        unset(self::$timers[$metric]);
-
+        $duration = self::getDuration($metric);
         self::$totals[$metric]['count'] += 1;
-        self::$totals[$metric]['time'] += self::getDurationInMicroseconds($time);
+        self::$totals[$metric]['time'] += $duration['microseconds'];
 
-        return self::getDurationInMilliseconds($time);
+        // The metric name is used as is in the Server-Timing header, so we need to sanitize it
+        // to avoid issues with special characters.
+        $metric = preg_replace('/[^a-zA-Z0-9\s]/', '', $metric);
+        $metric = str_replace(' ', '_', $metric);
+
+        header('Server-Timing: ' . $metric . ';dur=' . $duration['milliseconds'] . ';', false);
+        
+        return $duration['milliseconds'];
     }
 
     /**
@@ -71,61 +104,108 @@ class ServerTiming
      */
     public static function profile(string $metric): void
     {
-        if (isset(self::$timers[$metric])) {
-            $durationInMicroseconds = self::stop($metric);
-            header('Server-Timing: ' . $metric . ';dur=' . $durationInMicroseconds . ';desc=' . $metric);
-        } else {
+        if (!isset(self::$timers[$metric])) {
             self::start($metric);
+            return;
         }
+
+        $duration = self::stop($metric);
     }
 
     /**
      * Calculates the time between the start and stop calls and logs the duration.
      *
      * @param string $metric The name of the metric.
+     * @param bool $debug If true, the start time will be printed.
      * @return void
      */
-    public static function log(string $metric): void
+    public static function log(string $metric, bool $debug = true): void
     {
-        if (isset(self::$timers[$metric])) {
-            $durationInMicroseconds = self::stop($metric);
-            $count = self::$totals[$metric]['count'];
-            $totalTimeInMicroseconds = self::$totals[$metric]['time'];
-            $durationInMilliseconds = self::getDurationInMilliseconds($durationInMicroseconds);
-            $totalTimeInMilliseconds = self::getDurationInMilliseconds($totalTimeInMicroseconds);
-            error_log(sprintf(
-                'Server-Timing: %s stop - (%s ms - called %d times, total time: %s ms)',
-                $metric,
-                round($durationInMilliseconds, 5),
-                $count,
-                round($totalTimeInMilliseconds, 5)
-            ));
-        } else {
-            self::start($metric);
-            error_log('Server-Timing: ' . $metric . " start");
+        if (!self::$logFile) {
+            self::detectLogFile();
         }
+
+        if (!isset(self::$timers[$metric])) {
+            self::start($metric);
+            if ($debug) {
+                self::writeLog('Server-Timing: ' . $metric);
+            }
+            return;
+        }
+
+        $duration = self::stop($metric);
+        $count = self::$totals[$metric]['count'];
+        $totalTime = self::getDurationInMilliseconds(self::$totals[$metric]['time']);
+
+        self::writeLog(sprintf(
+            'Server-Timing: %s - (%s ms - called %d times, total time: %s ms)',
+            $metric,
+            round($duration, 5),
+            $count,
+            round($totalTime, 5)
+        ));
     }
 
+    /**
+     * Writes a message to the log file.
+     *
+     * @param string $message The message to be written to the log file.
+     * @return void
+     */
+    private static function writeLog(string $message): void
+    {
+        file_put_contents(self::$logFile, $message . PHP_EOL, FILE_APPEND);
+    }
 
     /**
-     * Calculates the duration in microseconds between the given start time and the current time.
+     * Sets the path to the log file.
      *
-     * @param float $startTime The start time in seconds with microseconds.
-     * @return float The duration in microseconds.
+     * This method sets the path to the log file where the ServerTiming metrics will be written.
+     * It checks if the file exists and if it is writable. If the file does not exist or is not writable,
+     * it throws an exception.
+     *
+     * @param string $path The path to the log file.
+     * @return void
+     * @throws \Exception If the file does not exist or is not writable.
      */
-    private static function getDurationInMicroseconds(float $startTime): float
+    public static function setLogFile(string $path): void
     {
-        return (microtime(true) - $startTime) * 1000000;
+        if (!file_exists($path)) {
+            throw new \Exception("ServerTiming exception: The file does not exist: $path");
+        }
+        if (!is_writable($path)) {
+            throw new \Exception("ServerTiming exception: The file exists but is not writable: $path");
+        }
+
+        self::$logFile = $path;
+    }
+
+    /**
+     * Calculates the duration in microseconds and milliseconds between the given start time and the current time.
+     *
+     * @param string $metric The name of the metric.
+     * @return array The duration in microseconds and milliseconds.
+     */
+    private static function getDuration(string $metric): array
+    {
+        $startTime = self::$timers[$metric];
+        unset(self::$timers[$metric]);
+
+        $durationInMicroseconds = (microtime(true) - $startTime) * 1000000;
+        return [
+            'microseconds' => $durationInMicroseconds,
+            'milliseconds' => $durationInMicroseconds / 1000
+        ];
     }
 
     /**
      * Calculates the duration in milliseconds between the given start time and the current time.
      *
-     * @param float $startTime The start time in seconds with microseconds.
+     * @param float $microseconds The duration in microseconds.
      * @return float The duration in milliseconds.
      */
-    private static function getDurationInMilliseconds(float $startTime): float
+    private static function getDurationInMilliseconds(float $microseconds): float
     {
-        return self::getDurationInMicroseconds($startTime) / 1000;
+        return $microseconds / 1000;
     }
 }
